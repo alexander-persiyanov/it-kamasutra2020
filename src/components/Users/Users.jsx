b965e9b58e6372cff7f2019496deaf5e090a494c
Alexander Persiyanov <persiyanov1990@hotmail.com>
2021-02-05 19:44:47 +0100

    54 users added componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -8,35 +8,34 @@ import UserItem from './UserItem/UserItem';
 
 class Users extends React.Component {
 
-    getUsers = ()=>{
+    componentDidMount() {
 
-        if(this.props.users.length ===0){
-
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").
-            then((response)=>{
-                
-                this.props.setUsers(response.data.items);
-    
-            });
-        }
+        this.getUsers();
 
     }
 
+    getUsers = () => {
 
-    render(){
-        let usersElements = this.props.users.map((u)=>{ return <UserItem key={u.id} user={u} follow={this.props.follow} unfollow={this.props.unfollow} defaultAvatar={userDefaultAvatar} />;});
-        
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").
+            then((response) => {
+                this.props.setUsers(response.data.items);
+        });
+    }
+
+
+    render() {
+        let usersElements = this.props.users.map((u) => { return <UserItem key={u.id} user={u} follow={this.props.follow} unfollow={this.props.unfollow} defaultAvatar={userDefaultAvatar} />; });
+
         return (
             <div className={s.users}>
-    
+
                 <h2>Users List</h2>
                 <div>
                     {usersElements}
                 </div>
-                <button onClick={this.getUsers}>Set Users</button>
-    
+               
             </div>
-        );      
+        );
     }
 
 }
