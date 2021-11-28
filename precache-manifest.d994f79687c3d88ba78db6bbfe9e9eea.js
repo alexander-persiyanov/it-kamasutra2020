24b962e2fb98e4eba3bc2d554f7eccf5a0d26cf6
Alexander Persiyanov <persiyanov1990@hotmail.com>
2021-11-28 18:24:35 +0100

    Updates

---
+++ precache-manifest.d994f79687c3d88ba78db6bbfe9e9eea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3001c1df0a82894e6f9f854aea4e6c49",
+    "url": "/it-kamasutra2020/index.html"
+  },
+  {
+    "revision": "65a1e7cf11f1f179135b",
+    "url": "/it-kamasutra2020/static/css/3.537d3896.chunk.css"
+  },
+  {
+    "revision": "ddfec61dadcb32551f97",
+    "url": "/it-kamasutra2020/static/css/main.0963fb57.chunk.css"
+  },
+  {
+    "revision": "1f7fc8a240437b1126a3",
+    "url": "/it-kamasutra2020/static/js/2.1eb80a44.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/it-kamasutra2020/static/js/2.1eb80a44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65a1e7cf11f1f179135b",
+    "url": "/it-kamasutra2020/static/js/3.2708948a.chunk.js"
+  },
+  {
+    "revision": "ddfec61dadcb32551f97",
+    "url": "/it-kamasutra2020/static/js/main.39cad078.chunk.js"
+  },
+  {
+    "revision": "590dd61d9e38cf92ccc5",
+    "url": "/it-kamasutra2020/static/js/runtime-main.deef849e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/it-kamasutra2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
